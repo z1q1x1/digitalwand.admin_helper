5aecfb9e5b99575f2120cf28d0911e8714d6de9b
Vladimir Manskov <z1q1x1@yandex.ru>
2024-12-24 13:49:30 +0300

    Исправление проблемы при попытке удалить элемент из списка

--- lib/helper/AdminListHelper.php
+++ lib/helper/AdminListHelper.php
@@ -1686,7 +1686,7 @@ abstract class AdminListHelper extends AdminBaseHelper
 			$primary = $className::getEntity()->getPrimary();
 		}
 
-		if (count($primary) === 1) {
+		if (count($primary) === 1 || !is_array($primary)) {
 			return array($this->pk() => $id);
 		}
 
